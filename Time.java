ef452241542317c7369ee70e012c009787d75428
cubicn101 <yuxinxia702@gmail.com>
2026-09-16 14:27:07 -0700

    Add files via upload

---
+++ Time.java
@@ -0,0 +1,17 @@
+public class Time {
+     
+     public static void main (String [] args) {
+     int day = 16;
+     int month = 9;
+     int year = 2026;
+     System.out.print( day + "/"+ month + "/" + year);
+     // i lost my orignial package of it and i redo it today :( I dont know why it failed to save in github
+
+
+
+
+
+
+
+
+}}
