8e4c535724bd3a6540e3fa34d054fde83c2fbb9a
cubicn101 <yuxinxia702@gmail.com>
2026-09-16 14:37:23 -0700

    Add files via upload

---
+++ Time.java
@@ -0,0 +1,15 @@
+public class Time {
+     
+     public static void main (String [] args) {
+     int hour = 14;
+     int minute = 31;
+     System.out.println( hour * 3600+ minute * 60);
+     
+     
+     
+     
+     
+     
+     
+     }
+     }
